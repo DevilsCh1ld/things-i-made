96fc771e9673b6322cc46ad652685a7bdcb41eb1
Dago <hellbound12200@gmail.com>
2024-07-24 07:36:48 +0000

    Little Calculator
    my first serious "Program"

---
+++ Calculadora.py
@@ -0,0 +1,35 @@
+print("""
+      Bienvenido a la calculadora
+      Para salir escribe Salir
+      Las Operaciones disponibles son suma, multi, div y resta
+""")
+
+resultado = ""
+while True:
+    if not resultado:
+        resultado = input("Ingrese un numero: ")
+        if resultado.lower() == "salir":
+            break
+    resultado = int(resultado)
+    op = input("Ingresar Operacion: ")
+    if op.lower() == "salir":
+            break
+    n2 = (input("Ingrese el Segundo Numero: "))
+    if n2.lower() == "salir":
+            break
+    n2 = int(n2)
+    if op.lower() == "suma":
+       suma = resultado + n2
+       print(f"El resultado es: {suma}")
+    elif op.lower() == "multi":
+        multi = resultado * n2
+        print(f"El resultado es: {multi}")
+    elif op.lower() == "div":
+        div = resultado // n2
+        print(f"El resultado es: {div}")
+    elif op.lower() == "resta":
+        resta = resultado - n2
+        print(f"El resultado es: {resta}")
+    else:
+        print("Operacion no valida")
+        break     
